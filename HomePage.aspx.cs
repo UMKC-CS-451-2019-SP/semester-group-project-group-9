ff2d90aa7663716364ee8703b9587f560d450898
saally61 <37127822+saally61@users.noreply.github.com>
2019-03-18 20:34:45 -0500

    Homepage
    Front end prototype for Home Page

---
+++ HomePage.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Flashcards_Project
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void Create_Click(object sender, EventArgs e)
+        {
+            //sender = "Create" button
+            return;
+        }
+    }
+}
